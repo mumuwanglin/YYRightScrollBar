ca9c378af9620f1352322ccb764778f0e3f61797
王林 <wanglin@km.com>
2020-09-09 15:27:43 +0800

    Initial Commit

---
+++ YYRightScrollBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YYRightScrollBar
+//
+//  Created by mumu on 2020/9/9.
+//  Copyright © 2020 mumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
